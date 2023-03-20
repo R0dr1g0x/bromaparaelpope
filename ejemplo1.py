be1e9ec8615713e9ab0405e8f4d1c54c31c878a3
R0dr1g0x <126715338+R0dr1g0x@users.noreply.github.com>
2023-03-20 18:49:15 -0300

    Add files via upload

---
+++ ejemplo1.py
@@ -0,0 +1,31 @@
+print("hola chicos")
+print("yo soy rodrigo solar")
+print("y hoy estoy editando en idle")
+print("intentare crear una serie de programas")
+print("utilizando distintas lineas de codigo sea while,if o etc.")
+print("comenzare utilizando el comando while")
+print("primero le dare un valor a x y luego trabajare while con ella")
+
+
+print("le hare una serie de preguntas ahora")
+print("conteste s (si) o n (no) a las siguientes preguntas: ")
+
+uno = input("¿Al pope le gusta solo 1 mujer?")
+if uno == "s":
+    dos = input("¿Usted esta seguro de su respuesta?")
+    if dos == "n":
+        print("Usted es un tonto")
+    else :
+        print("Usted es un palurdo")
+else:
+    dos = input("¿Al pope le gustan todas?")
+    if dos == "s":
+        print("Usted debe ser el mejor amigo del pope")
+    else :
+        print("Usted no sabe nada")
+
+
+print("Nos vemos")
+
+    
+ 
